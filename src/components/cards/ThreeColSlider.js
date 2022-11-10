aea3e975bca5b4a4e071e45289b054d5e5a1fca0
Neelmani Singh <nsingh@uplandsoftware.com>
2022-11-10 18:29:05 +0530

    active campaigns

--- src/components/cards/ThreeColSlider.js
+++ src/components/cards/ThreeColSlider.js
@@ -7,8 +7,12 @@ import { PrimaryButton as PrimaryButtonBase } from "components/misc/Buttons";
 import { ReactComponent as PriceIcon } from "feather-icons/dist/icons/dollar-sign.svg";
 import { ReactComponent as LocationIcon } from "feather-icons/dist/icons/map-pin.svg";
 import { ReactComponent as StarIcon } from "feather-icons/dist/icons/star.svg";
+import {ReactComponent as ClockIcon} from "feather-icons/dist/icons/clock.svg";
+import {ReactComponent as HeartIcon} from "feather-icons/dist/icons/heart.svg";
 import { ReactComponent as ChevronLeftIcon } from "feather-icons/dist/icons/chevron-left.svg";
 import { ReactComponent as ChevronRightIcon } from "feather-icons/dist/icons/chevron-right.svg";
+import ProgressBar from "./ProgressBar";
+import DonateModal from "./DonateModal";
 
 const Container = tw.div`relative`;
 const Content = tw.div`max-w-screen-xl mx-auto py-16 lg:py-20`;
@@ -37,12 +41,12 @@ const CardSlider = styled(Slider)`
 const Card = tw.div`h-full flex! flex-col sm:border max-w-sm sm:rounded-tl-4xl sm:rounded-br-5xl relative focus:outline-none`;
 const CardImage = styled.div(props => [
   `background-image: url("${props.imageSrc}");`,
-  tw`w-full h-56 sm:h-64 bg-cover bg-center rounded sm:rounded-none sm:rounded-tl-4xl`
+  tw`w-full h-56 sm:h-64 bg-cover bg-center`
 ]);
 
 const TextInfo = tw.div`py-6 sm:px-10 sm:py-6`;
 const TitleReviewContainer = tw.div`flex flex-col sm:flex-row sm:justify-between sm:items-center`;
-const Title = tw.h5`text-2xl font-bold`;
+const Title = tw.h5`text-lg font-bold h-24`;
 
 const RatingsInfo = styled.div`
   ${tw`flex items-center sm:ml-4 mt-2 sm:mt-0`}
@@ -57,14 +61,19 @@ const Description = tw.p`text-sm leading-loose mt-2 sm:mt-4`;
 const SecondaryInfoContainer = tw.div`flex flex-col sm:flex-row mt-2 sm:mt-4`;
 const IconWithText = tw.div`flex items-center mr-6 my-2 sm:my-0`;
 const IconContainer = styled.div`
-  ${tw`inline-block rounded-full p-2 bg-gray-700 text-gray-100`}
   svg {
-    ${tw`w-3 h-3`}
+    ${tw`w-6 h-6`}
   }
 `;
-const Text = tw.div`ml-2 text-sm font-semibold text-gray-800`;
+const HeartIconContainer = styled.div`
+  ${tw`text-red-600`}
+  svg {
+    ${tw`w-6 h-6`}
+  }
+`
+const Text = tw.div`ml-2 text-xs font-semibold text-gray-800`;
 
-const PrimaryButton = tw(PrimaryButtonBase)`mt-auto sm:text-lg rounded-none w-full rounded sm:rounded-none sm:rounded-br-4xl py-3 sm:py-6`;
+const PrimaryButton = tw(PrimaryButtonBase)`mt-auto sm:text-lg rounded-none w-full py-3 sm:py-6`;
 export default () => {
   // useState is used instead of useRef below because we want to re-render when sliderRef becomes available (not null)
   const [sliderRef, setSliderRef] = useState(null);
@@ -92,40 +101,58 @@ export default () => {
   const cards = [
     {
       imageSrc: "https://images.unsplash.com/photo-1571896349842-33c89424de2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
-      title: "Wyatt Residency",
-      description: "Lorem ipsum dolor sit amet, consectur dolori adipiscing elit, sed do eiusmod tempor nova incididunt ut labore et dolore magna aliqua.",
-      locationText: "Rome, Italy",
-      pricingText: "USD 39/Day",
+      description: "Wyatt Residency",
+      title: "My Baby Battles For His Life And We Need Your Support To Save Him",
+      daysLeft: "38 Days Left",
+      supporters: "549 Supporters",
+      fundsRaised:"95,000",
+      fundsNeeded:"1,00,000",
+      completedPercent: "95",
       rating: "4.8",
     },
     {
       imageSrc: "https://images.unsplash.com/photo-1571003123894-1f0594d2b5d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
-      title: "Soho Paradise",
-      description: "Lorem ipsum dolor sit amet, consectur dolori adipiscing elit, sed do eiusmod tempor nova incididunt ut labore et dolore magna aliqua.",
-      locationText: "Ibiza, Spain",
-      pricingText: "USD 50/Day",
+      description: "Soho Paradise",
+      title: "Help End Isolation for Children In Need: Empowering Holistically",
+      daysLeft: "39 Days Left",
+      supporters: "205 Supporters",
+      fundsRaised:"15,000",
+      fundsNeeded:"1,00,000",
+      completedPercent: "15",
       rating: 4.9,
     },
     {
       imageSrc: "https://images.unsplash.com/photo-1549294413-26f195200c16?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
-      title: "Hotel Baja",
-      description: "Lorem ipsum dolor sit amet, consectur dolori adipiscing elit, sed do eiusmod tempor nova incididunt ut labore et dolore magna aliqua.",
-      locationText: "Palo Alto, CA",
-      pricingText: "USD 19/Day",
+      description: "Hotel Baja",
+      title: "Help 14-year-old Anil to experience a beautiful life ahead!",
+      daysLeft: "9 Days Left",
+      supporters: "238 Supporters",
+      fundsRaised:"60,000",
+      fundsNeeded:"1,00,000",
+      completedPercent: "60",
       rating: "5.0",
     },
     {
       imageSrc: "https://images.unsplash.com/photo-1571770095004-6b61b1cf308a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
       title: "Hudak Homes",
       description: "Lorem ipsum dolor sit amet, consectur dolori adipiscing elit, sed do eiusmod tempor nova incididunt ut labore et dolore magna aliqua.",
-      locationText: "Arizona, RAK",
-      pricingText: "USD 99/Day",
+      daysLeft: "Arizona, RAK",
+      supporters: "USD 99/Day",
       rating: 4.5,
     },
   ]
 
+  const [showDonateModal, setShowDonateModal] = useState(false);
+
+  const closeModal = ()=>{
+    setShowDonateModal(false)
+  }
+
   return (
     <Container>
+      {showDonateModal ? (
+          <DonateModal modalIsOpen={showDonateModal} closeModal={closeModal}/>
+      ) : null}
       <Content>
         <HeadingWithControl>
           <Heading>Active Campaigns</Heading>
@@ -141,28 +168,33 @@ export default () => {
               <TextInfo>
                 <TitleReviewContainer>
                   <Title>{card.title}</Title>
-                  <RatingsInfo>
-                    <StarIcon />
-                    <Rating>{card.rating}</Rating>
-                  </RatingsInfo>
+                  {/*<RatingsInfo>*/}
+                  {/*  <StarIcon />*/}
+                  {/*  <Rating>{card.rating}</Rating>*/}
+                  {/*</RatingsInfo>*/}
                 </TitleReviewContainer>
+                <ProgressBar
+                    progressPercentage={card.completedPercent}
+                    fundsRaised={card.fundsRaised}
+                    fundsNeeded={card.fundsNeeded}
+                />
                 <SecondaryInfoContainer>
                   <IconWithText>
                     <IconContainer>
-                      <LocationIcon />
+                      <ClockIcon />
                     </IconContainer>
-                    <Text>{card.locationText}</Text>
+                    <Text>{card.daysLeft}</Text>
                   </IconWithText>
                   <IconWithText>
-                    <IconContainer>
-                      <PriceIcon />
-                    </IconContainer>
-                    <Text>{card.pricingText}</Text>
+                    <HeartIconContainer>
+                      <HeartIcon />
+                    </HeartIconContainer>
+                    <Text>{card.supporters}</Text>
                   </IconWithText>
                 </SecondaryInfoContainer>
-                <Description>{card.description}</Description>
+                {/*<Description>{card.description}</Description>*/}
               </TextInfo>
-              <PrimaryButton>Book Now</PrimaryButton>
+              <PrimaryButton onClick={() => setShowDonateModal(true)}>Donate Now</PrimaryButton>
             </Card>
           ))}
         </CardSlider>
