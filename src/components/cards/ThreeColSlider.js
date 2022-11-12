ca93abe2ae4165729328adae0d1f05d02494b9c8
neelmani098 <neelmani98@outlook.com>
2022-11-12 16:39:04 +0530

    donate modal & Login Route

--- src/components/cards/ThreeColSlider.js
+++ src/components/cards/ThreeColSlider.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import Slider from "react-slick";
 import tw from "twin.macro";
+import axios from "axios";
 import styled from "styled-components";
 import { SectionHeading } from "components/misc/Headings";
 import { PrimaryButton as PrimaryButtonBase } from "components/misc/Buttons";
@@ -96,57 +97,20 @@ export default () => {
       },
     ]
   };
+  const [campaigns, setCampaigns] = useState([]);
 
-  /* Change this according to your needs */
-  const cards = [
-    {
-      imageSrc: "https://images.unsplash.com/photo-1571896349842-33c89424de2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
-      description: "Wyatt Residency",
-      title: "My Baby Battles For His Life And We Need Your Support To Save Him",
-      daysLeft: "38 Days Left",
-      supporters: "549 Supporters",
-      fundsRaised:"95,000",
-      fundsNeeded:"1,00,000",
-      completedPercent: "95",
-      rating: "4.8",
-    },
-    {
-      imageSrc: "https://images.unsplash.com/photo-1571003123894-1f0594d2b5d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
-      description: "Soho Paradise",
-      title: "Help End Isolation for Children In Need: Empowering Holistically",
-      daysLeft: "39 Days Left",
-      supporters: "205 Supporters",
-      fundsRaised:"15,000",
-      fundsNeeded:"1,00,000",
-      completedPercent: "15",
-      rating: 4.9,
-    },
-    {
-      imageSrc: "https://images.unsplash.com/photo-1549294413-26f195200c16?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
-      description: "Hotel Baja",
-      title: "Help 14-year-old Anil to experience a beautiful life ahead!",
-      daysLeft: "9 Days Left",
-      supporters: "238 Supporters",
-      fundsRaised:"60,000",
-      fundsNeeded:"1,00,000",
-      completedPercent: "60",
-      rating: "5.0",
-    },
-    {
-      imageSrc: "https://images.unsplash.com/photo-1571770095004-6b61b1cf308a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
-      title: "Hudak Homes",
-      description: "Lorem ipsum dolor sit amet, consectur dolori adipiscing elit, sed do eiusmod tempor nova incididunt ut labore et dolore magna aliqua.",
-      daysLeft: "Arizona, RAK",
-      supporters: "USD 99/Day",
-      rating: 4.5,
-    },
-  ]
+  useEffect(()=>{
+    axios.get('http://127.0.0.1:5000')
+        .then(response=>setCampaigns(response.data))
+  },[])
+
 
   const [showDonateModal, setShowDonateModal] = useState(false);
 
   const closeModal = ()=>{
     setShowDonateModal(false)
   }
+
 
   return (
     <Container>
@@ -162,34 +126,34 @@ export default () => {
           </Controls>
         </HeadingWithControl>
         <CardSlider ref={setSliderRef} {...sliderSettings}>
-          {cards.map((card, index) => (
-            <Card key={index}>
-              <CardImage imageSrc={card.imageSrc} />
+          {campaigns.map((campaign, index) => (
+            <Card key={campaign.campaignId}>
+              <CardImage imageSrc="https://images.unsplash.com/photo-1571896349842-33c89424de2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80" />
               <TextInfo>
                 <TitleReviewContainer>
-                  <Title>{card.title}</Title>
+                  <Title>{campaign.campaignName}</Title>
                   {/*<RatingsInfo>*/}
                   {/*  <StarIcon />*/}
                   {/*  <Rating>{card.rating}</Rating>*/}
                   {/*</RatingsInfo>*/}
                 </TitleReviewContainer>
                 <ProgressBar
-                    progressPercentage={card.completedPercent}
-                    fundsRaised={card.fundsRaised}
-                    fundsNeeded={card.fundsNeeded}
+                    progressPercentage={campaign.completedPercent}
+                    fundsRaised={campaign.campaignCollectedAmount}
+                    fundsNeeded={campaign.campaignTotalAmount}
                 />
                 <SecondaryInfoContainer>
                   <IconWithText>
                     <IconContainer>
                       <ClockIcon />
                     </IconContainer>
-                    <Text>{card.daysLeft}</Text>
+                    <Text>{campaign.daysLeft}</Text>
                   </IconWithText>
                   <IconWithText>
                     <HeartIconContainer>
                       <HeartIcon />
                     </HeartIconContainer>
-                    <Text>{card.supporters}</Text>
+                    <Text>{`${campaign.donators.length} Supporters`}</Text>
                   </IconWithText>
                 </SecondaryInfoContainer>
                 {/*<Description>{card.description}</Description>*/}
